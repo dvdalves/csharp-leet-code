b097f755a3e4d11398fe4567d4f6fef74f7a9031
David Alves <david.a0595@gmail.com>
2024-10-28 23:21:20 -0300

    Add project files.

---
+++ 1 - Easy/TwoSums.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode._1___Easy;
+
+public class TwoSums
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        var dict = new Dictionary<int, int>();
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            var diff = target - nums[i];
+
+            if (dict.TryGetValue(diff, out int value))
+            {
+                return [value, i];
+            }
+
+            dict.TryAdd(nums[i], i);
+        }
+
+        return [];
+    }
+}
