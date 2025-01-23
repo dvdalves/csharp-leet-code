2385a346abb06ced7d215b463ee0e716e1dc2fc2
David Alves <david.a0595@gmail.com>
2025-01-23 00:00:02 -0300

    RomanToInteger

---
+++ 1 - Easy/RomanToInteger.cs
@@ -0,0 +1,39 @@
+﻿namespace LeetCode._1___Easy;
+
+public class RomanToInteger
+{
+    public int RomanToInt(string s)
+    {
+        var total = 0;
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (i + 1 < s.Length && GetRomanValue(s[i]) < GetRomanValue(s[i + 1]))
+            {
+                total += GetRomanValue(s[i + 1]) - GetRomanValue(s[i]);
+                i++;
+            }
+            else
+            {
+                total += GetRomanValue(s[i]);
+            }
+        }
+
+        return total;
+    }
+
+    private int GetRomanValue(char romanChar)
+    {
+        return romanChar switch
+        {
+            'I' => 1,
+            'V' => 5,
+            'X' => 10,
+            'L' => 50,
+            'C' => 100,
+            'D' => 500,
+            'M' => 1000,
+            _ => throw new ArgumentException("Invalid Roman numeral character")
+        };
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,19 +1,24 @@
 ﻿using LeetCode._1___Easy;
 
-namespace LeetCode
+namespace LeetCode;
+
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            //var twoSums = new TwoSums();
-            //var result = twoSums.TwoSum([2, 7, 11, 15], 26);
-            //Console.WriteLine(result);
+        //var twoSums = new TwoSums();
+        //var result = twoSums.TwoSum([2, 7, 11, 15], 26);
+        //Console.WriteLine(result);
 
-            var palindrome = new PalindromeNumber();
-            PalindromeNumber.IsPalindrome(-121);
+        //var palindrome = new PalindromeNumber();
+        //PalindromeNumber.IsPalindrome(-121);
 
+        var word = Console.ReadLine();
 
-        }
+        var romanToInteger = new RomanToInteger();
+
+        var retorno = romanToInteger.RomanToInt(word);
+        Console.WriteLine(retorno);
+
     }
 }
